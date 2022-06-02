327d10201af89a6241ef9a66b0cb6b1647f7097c
Yi Wang <48071660+yimango@users.noreply.github.com>
2022-06-01 22:22:15 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        towersOfHanoi(3, "1", "2", "3");
+    }
+
+    public static void towersOfHanoi(int n, String startRod, String utilityRod, String endRod) {
+        if (n == 1) {
+            System.out.println("Move disk " + n + " from rod " + startRod + " to rod " + endRod);
+            return;
+        }
+        towersOfHanoi(n - 1, startRod, endRod, utilityRod);
+        System.out.println("Move disk " + n + " from rod " + startRod + " to rod " + endRod);
+        towersOfHanoi(n - 1, utilityRod, startRod, endRod);
+    }
+}
